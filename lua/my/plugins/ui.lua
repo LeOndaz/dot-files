60d8aeb38015a16717ff62c46e4c06f0b76ca11e
janw4ld <janw4ld@tutanota.com>
2024-11-18 11:15:53 +0200

    feat: add more ui goodness

--- lua/my/plugins/ui.lua
+++ lua/my/plugins/ui.lua
@@ -8,4 +8,31 @@ return {
       vim.cmd 'colorscheme moonfly'
     end,
   },
+
+  { -- indentation guides
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = { indent = { char = '▏' } },
+  },
+
+  { -- highlight colors in hex, rgb, etc. example: #bb55f0
+    'norcalli/nvim-colorizer.lua',
+    config = function(_)
+      require('colorizer').setup()
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufNewFile' }, {
+        command = 'ColorizerAttachToBuffer',
+        group = vim.api.nvim_create_augroup(
+          'my-colorizer-attach',
+          { clear = true }
+        ),
+      })
+    end,
+  },
+
+  { -- highlight TODO, FIXME, etc prefixes in comments
+    'folke/todo-comments.nvim', -- NOTE: example!
+    opts = { sign_priority = 5 },
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
 }
